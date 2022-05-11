1a40177bcdf8c489940064a23e001df5470f93c0
michelle-sokolov <michellersokolov@gmail.com>
2022-05-10 22:30:30 -0700

    pieces updated

--- myNewApp/src/app/app-routing.module.ts
+++ myNewApp/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DataTableComponent } from "./data-table/data-table.component";
-import {DetailsComponent} from './details/details.component';
+// import { DataTableComponent } from "./data-table/data-table.component";
+// import {DetailsComponent} from './details/details.component';
 
 const routes: Routes = [
-  { path: 'app-data-table', component: DataTableComponent },
-  { path: 'app-details/:id', component: DetailsComponent },
+ // { path: 'app-data-table', component: DataTableComponent },
+//  { path: 'app-details/:id', component: DetailsComponent },
 ];
 
 @NgModule({
--- myNewApp/src/app/app.module.ts
+++ myNewApp/src/app/app.module.ts
@@ -3,16 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PhotoGalleryComponent } from './photo-gallery/photo-gallery.component';
-import { DataTableComponent } from './data-table/data-table.component';
-import { DetailsComponent } from './details/details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PhotoGalleryComponent,
-    DataTableComponent,
-    DetailsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
